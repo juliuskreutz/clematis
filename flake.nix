d81f254ea4f0975617a831e6a4d7ffbe58fd242f
Julius Kreutz <julius@kreutz.dev>
2025-05-01 21:18:06 +0200

    testing

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     in
     {
       nixosConfigurations.${routes.hostName} = inputs.nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
+        system = "x86_64-linux";
         specialArgs = { inherit inputs routes; };
         modules = [
           # Hostinger specific configurations
